4796d126a7b800649ed4732602468b6d8140be6c
Azmir Uddin Alif <79633608+Azmir02@users.noreply.github.com>
2022-12-17 23:25:21 +0600

    SYE

--- backend/handaler/Varification.js
+++ backend/handaler/Varification.js
@@ -1,31 +1,32 @@
-const User = require('../models/UserModel')
+const User = require("../models/UserModel");
 
-exports.varifyEmail = (email)=>{
-    return String(email).toLowerCase().match(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/);
-}
+exports.varifyEmail = (email) => {
+  return String(email)
+    .toLowerCase()
+    .match(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/);
+};
 
-exports.validateLength = (text,min,max) =>{
-    if(text.length < min && text.length > max) {
-        return false;
-    }else{
-        return true;
+exports.validateLength = (text, min, max) => {
+  if (text.length < min || text.length > max) {
+    return false;
+  } else {
+    return true;
+  }
+};
+
+exports.validateUsername = async (username) => {
+  let isTrue = false;
+
+  do {
+    let user = await User.findOne({ username });
+    // check existing_Users in DB
+    if (user) {
+      username += (+new Date() * Math.random()).toString().substring(0, 1);
+      isTrue = true;
+    } else {
+      isTrue = false;
     }
-}
+  } while (isTrue);
 
-exports.validateUsername = async (username)=>{
-    let isTrue = false
-
-    do{
-        let user = await User.findOne({ username })
-        // check existing_Users in DB
-        if(user) {
-            username += (+new Date() * Math.random()).toString().substring(0,1)
-            isTrue = true
-        }else{
-            isTrue = false
-        }
-    }
-    while(isTrue)
-
-    return username
-}
+  return username;
+};
