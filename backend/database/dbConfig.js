c15307007dbad5a50b3cef7d2378e2184751f4c9
Azmir Uddin Alif <azmir02alif@gmail.com>
2023-07-29 16:03:59 +0600

    cluster-change

--- backend/database/dbConfig.js
+++ backend/database/dbConfig.js
@@ -1,8 +1,24 @@
-require('dotenv').config();
-const mongoose = require('mongoose')
+require("dotenv").config();
+const mongoose = require("mongoose");
 
-exports.connect = ()=>{
-    mongoose.connect(process.env.MONGODB_URL,()=>{
-        console.log("database connected");
-    });
-}
+exports.connect = () => {
+  mongoose.connect(process.env.MONGODB_URL, () => {
+    console.log("database connected");
+  });
+};
+
+// mongoose.connect(process.env.DB_CONNECT, { useNewUrlParser: true }, () => { console.log('Connected to MongoDB'); });
+// exports.connect = async () => {
+//   try {
+//     mongoose.set("useNewUrlParser", true);
+
+//     await mongoose.connect(process.env.MONGODB_URL);
+
+//     console.log("connected to database");
+//   } catch (error) {
+//     console.log(error);
+//     process.exit(1);
+//   }
+// };
+
+// connect();
